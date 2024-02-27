d9c37aeab855b93b1a42dfb570fc5d3bb04d3af9
noble-shake <154754400+noble-shake@users.noreply.github.com>
2024-02-27 19:55:55 +0900

    no message

--- 3D Ani/Assets/Scripts/DayNightManager.cs
+++ 3D Ani/Assets/Scripts/DayNightManager.cs
@@ -11,7 +11,10 @@ public class DayNightManager : MonoBehaviour
     private string keyIsNight = "isNight";
     public bool AutoChange = false;//자동 낮밤 변경
 
-    List<FunctionLight>listLight = new List<FunctionLight>();
+    [SerializeField,Range(0,24f)] float dayTime = 14;
+    [SerializeField,Range(0,24f)] float nightTime = 24;
+
+    List<FunctionLight> listLight = new List<FunctionLight>();
 
 
     private void OnApplicationQuit()
@@ -24,21 +27,28 @@ public class DayNightManager : MonoBehaviour
     {
         string value = PlayerPrefs.GetString(keyIsNight, "f");
         isNight = value == "t" ? true : false;
-        if(directionalLight == null)
+        if (directionalLight == null)
         {
-            Light[]lights = GameObject.FindObjectsOfType<Light>();
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
             int count = lights.Length;
-            for(int iNum = 0; iNum < count; ++iNum)
+            for (int iNum = 0; iNum < count; ++iNum)
             {
                 Light light = lights[iNum];
-                if(light.type == LightType.Directional)
+                if (light.type == LightType.Directional)
                 {
                     directionalLight = light;
                 }
                 else
                 {
-                    listLight.Add(light.transform.GetComponent<FunctionLight>());
+                    listLight.Add(light.transform.parent.GetComponent<FunctionLight>());
                 }
+            }
+
+            count = listLight.Count;
+            for (int iNum = 0; iNum < count; ++iNum)
+            {
+                FunctionLight light = listLight[iNum];
+                light.init(isNight);
             }
         }
     }
@@ -46,26 +56,90 @@ public class DayNightManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         timeOfDay %= 24;//0~24 사이의 값만 저장
-        if(AutoChange == true)
+        if (AutoChange == true)
         {
             timeOfDay += Time.deltaTime;//시간이 자동으로 증가
         }
         else//시간을 직접 관리
         {
-            if(isNight == true)
+            if (isNight == true)
             {
-                timeOfDay = 19;//밤
+                timeOfDay += Time.deltaTime;
+                if (timeOfDay > dayTime)
+                {
+                    isNight = true;//밤
+                }
+                else if (timeOfDay > nightTime)
+                {
+                    isNight = false;
+                }
+
+                if (timeOfDay > 23) { }//모름 수정수정수정
             }
             else
             {
-                timeOfDay = 14;//낮
+                timeOfDay += Time.deltaTime;
+                if (timeOfDay < nightTime && timeOfDay > dayTime)
+                {
+                    timeOfDay = dayTime;//낮
+                }
             }
+
         }
+        updateLighting();
     }
+
+    private void updateLighting()
+    {
+        if (directionalLight == null)
+        {
+            Debug.Log("게임에 디렉셔널 라이트가 존재하지 않습니다");
+            return;
+        }
+
+        float timePercent = timeOfDay / 24f;//0~1
+        directionalLight.transform.localRotation = Quaternion.Euler(new Vector3(timePercent * 360f - 90f, 150f, 0));
+        if (directionalLight.transform.eulerAngles.x >= 180f && directionalLight.color.r > 0.0f)//밤
+        {
+
+            directionalLight.color -= new Color(1, 1, 1) * Time.deltaTime * 10f;
+            if (directionalLight.color.r < 0.0f)
+            {
+                directionalLight.color = new Color(0, 0, 0);
+
+                foreach (FunctionLight light in listLight)
+                {
+                    light.TurnOnLight(true);
+                }
+            }
+
+
+
+        }
+        else if (directionalLight.transform.localRotation.x >= 0.0f && directionalLight.color.r < 1.0f)//낮
+        {
+
+            directionalLight.color += new Color(1, 1, 1) * Time.deltaTime * 10f;
+            if (directionalLight.color.r > 1.0f)
+            {
+                directionalLight.color = new Color(1, 1, 1);
+
+                foreach (FunctionLight light in listLight)
+                {
+                    light.TurnOnLight(false);
+                }
+            }
+
+
+
+        }
+
+    }
+
 }
--- 3D Ani/Assets/Scripts/FunctionLight.cs
+++ 3D Ani/Assets/Scripts/FunctionLight.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 
 public class FunctionLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum TypeLight
     {
-        
+        Disable,//ºñ»ç¿ë
+        Allaways,//Ç×»ó
+        OnlyNight,//¹ã¿¡¸¸
+        OnlyDays,//³·¿¡¸¸
     }
 
-    // Update is called once per frame
-    void Update()
+    public TypeLight typeLight;
+
+    Material matWindow;
+    GameObject objLight;
+
+
+    public void init(bool _isNight)
     {
-        
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        matWindow = Instantiate(mr.material);
+        mr.material = matWindow;
+
+        objLight = GetComponentInChildren<Light>().gameObject;
+        //°°À½
+        //objLight = transform.GetChild(0).gameObject;
+        //objLight = transform.Find("Point Light").gameObject;
+
+
+        //matWindow.EnableKeyword("_EMISSION");//ÄÓ¶§
+        if (typeLight == TypeLight.Disable)
+        {
+
+            matWindow.DisableKeyword("_EMISSION");//²¨Áü
+            objLight.SetActive(false);
+        }
+        if ((_isNight == true && typeLight == TypeLight.OnlyNight) ||
+           (_isNight == false && typeLight == TypeLight.OnlyDays) ||
+           typeLight == TypeLight.Allaways)
+        {
+            TurnOnLight(true);
+        }
+        //else if(_isNight == false && typeLight == TypeLight.OnlyDays)
+        //{
+        //    matWindow.EnableKeyword("_EMISSION");
+        //    objLight.SetActive(true);
+        //}
+        //else if(typeLight == TypeLight.Allaways)
+        //{
+        //    matWindow.EnableKeyword("_EMISSION");
+        //    objLight.SetActive(true);
+        //}
+        else
+        {
+            TurnOnLight(false);
+        }
+    }
+
+    public void TurnOnLight(bool _value)
+    {
+        if(_value == true)
+        {
+            matWindow.EnableKeyword("_EMISSION");
+            objLight.SetActive(true);
+        }
+        else
+        {
+            matWindow.DisableKeyword("_EMISSION");//²¨Áü
+            objLight.SetActive(false);
+        }
     }
 }
